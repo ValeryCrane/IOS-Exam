1afeb84798e756a0f7198ad07d5b01579cfbfd4f
Valery Crane <zhulery@gmail.com>
2022-04-01 20:42:23 +0500

    Added CoreData

---
+++ Notes/CoreData/Note+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Note+CoreDataClass.swift
+//  Notes
+//
+//  Created by Валерий Журавлев on 01.04.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Note)
+public class Note: NSManagedObject {
+
+}
--- Notes/NotesWorker/NotesWorker.swift
+++ Notes/NotesWorker/NotesWorker.swift
@@ -6,22 +6,57 @@
 //
 
 import Foundation
+import CoreData
 
 class NotesWorker {
     static let shared = NotesWorker()
     private init() { }
     
-    private var notes: [NoteModel] = []
+    private let context: NSManagedObjectContext = {
+        let container = NSPersistentContainer(name: "NotesCoreData")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Container loading failed")
+            }
+        }
+        return container.viewContext
+    }()
+    
+    private func saveChanges() {
+        if context.hasChanges {
+            try? context.save()
+        }
+    }
     
     func add(note: NoteModel) {
-        notes.insert(note, at: 0)
+        let coreNote = Note(context: context)
+        coreNote.id = note.id
+        coreNote.title = note.title
+        coreNote.filling = note.filling
+        saveChanges()
     }
     
     func remove(note: NoteModel) {
-        notes = notes.filter { $0.id == note.id }
+        let fetchRequest: NSFetchRequest<Note> = Note.fetchRequest()
+        guard let coreNotes = try? context.fetch(fetchRequest) else { return }
+        
+        for coreNote in coreNotes {
+            if coreNote.id == note.id {
+                context.delete(coreNote)
+            }
+        }
+        
+        saveChanges()
     }
     
     func getNotes() -> [NoteModel] {
-        return notes
+        let fetchRequest: NSFetchRequest<Note> = Note.fetchRequest()
+        guard let coreNotes = try? context.fetch(fetchRequest) else { return [] }
+        return coreNotes.compactMap { coreNote in
+            guard let id = coreNote.id else { return nil }
+            return NoteModel(id: id,
+                             title: coreNote.title,
+                             filling: coreNote.filling)
+        }
     }
 }
