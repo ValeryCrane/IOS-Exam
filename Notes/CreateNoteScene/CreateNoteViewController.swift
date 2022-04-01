1810bea42b130c8bceccab78548111272978ef59
Valery Crane <zhulery@gmail.com>
2022-04-01 20:17:52 +0500

    Created CreateNoteViewController

---
+++ Notes/CreateNoteScene/CreateNoteViewController.swift
@@ -0,0 +1,59 @@
+//
+//  CreateNoteViewController.swift
+//  Notes
+//
+//  Created by Валерий Журавлев on 01.04.2022.
+//
+
+import Foundation
+import UIKit
+
+class CreateNoteViewController: UIViewController {
+    private let notesWorker = NotesWorker.shared
+    private let titleField = UITextField()
+    private let fillingView = UITextView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        configureLayout()
+        configureNavigationBar()
+    }
+    
+    private func configureLayout() {
+        titleField.placeholder = "Title"
+        titleField.font = .systemFont(ofSize: 32, weight: .bold)
+        fillingView.isEditable = true
+        fillingView.font = .systemFont(ofSize: 16)
+        
+        view.addSubview(titleField)
+        view.addSubview(fillingView)
+        
+        titleField.translatesAutoresizingMaskIntoConstraints = false
+        fillingView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            titleField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            titleField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 16),
+            
+            fillingView.topAnchor.constraint(equalTo: titleField.bottomAnchor),
+            fillingView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            fillingView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 8),
+            fillingView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 8)
+        ])
+    }
+    
+    private func configureNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Done", style: .done, target: self, action: #selector(addNote))
+    }
+    
+    @objc private func addNote() {
+        if let title = titleField.text, !title.isEmpty {
+            let filling = fillingView.text ?? ""
+            notesWorker.add(note: NoteModel(title: title, filling: filling))
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
--- Notes/SceneDelegate.swift
+++ Notes/SceneDelegate.swift
@@ -19,7 +19,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-        window.rootViewController = NotesAssembly.assemble()
+        let navigationController = UINavigationController(
+            rootViewController: NotesAssembly.assemble())
+        
+        window.rootViewController = navigationController
         self.window = window
         window.makeKeyAndVisible()
     }
